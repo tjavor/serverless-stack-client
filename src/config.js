125fc366365f4d605982354cb3495a71de2a9f43
Tamas Javor <tamas.javor@gmail.com>
2019-12-16 13:31:42 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-second-try"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://khpyvyexg6.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_jyeBMsnYh",
+    APP_CLIENT_ID: "1fgbjp12f7b6hmelqi5uc8ove3",
+    IDENTITY_POOL_ID: "us-east-2:71da8e8c-7c51-4ab4-bfa0-366f73a7c041"
+  }
+};
