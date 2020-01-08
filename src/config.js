33013817c1f3339bccaa3b7bb1b3cbfcdd96eac1
Tamas Javor <tamas.javor@gmail.com>
2020-01-07 23:00:29 -0600

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_s512CZwCyiz5RRBkPWc2oo4F00BO8o2n2I",
   s3: {
     REGION: "us-east-2",
     BUCKET: "notes-app-second-try"
